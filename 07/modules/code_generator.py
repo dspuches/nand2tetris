0eddd40071dd56a0d7741fafbb1e5ae0baa9c48d
Dan Spuches <dspuches@gmail.com>
2020-11-16 20:13:24 -0500

    Refactored asm generation into functions

--- 07/modules/code_generator.py
+++ 07/modules/code_generator.py
@@ -15,10 +15,10 @@ class CodeGenerator():
     # right now, all it does is initialize SP to 256
     def generate_preamble(self):
         asm_cmds = [
-            "@256",
-            "D=A",
-            "@SP",
-            "M=D",
+            "@256",             # A=256
+            "D=A",              # D=256
+            "@SP",              # A=SP
+            "M=D",              # M[SP]=256
         ]
         return asm_cmds
 
@@ -34,15 +34,15 @@ class CodeGenerator():
 
         asm_cmds = []
         switch = {
-            "add": self.a_add,
-            "sub": self.a_sub,
-            "neg": self.a_neg,
-            "eq": self.a_eq,
-            "gt": self.a_gt,
-            "lt": self.a_lt,
-            "and": self.a_and,
-            "or": self.a_or,
-            "not": self.a_not,
+            "add": self._a_add,
+            "sub": self._a_sub,
+            "neg": self._a_neg,
+            "eq": self._a_eq,
+            "gt": self._a_gt,
+            "lt": self._a_lt,
+            "and": self._a_and,
+            "or": self._a_or,
+            "not": self._a_not,
         }
         func = switch.get(command)
         return func()
@@ -57,186 +57,149 @@ class CodeGenerator():
 
         if command == config.C_PUSH:
             if segment == config.S_CONSTANT:
-                return self.asm_push_constant(index)
+                return self._asm_push_constant(index)
     
     # generates asm for the add VM arithemteic command
-    def a_add(self):
-        asm_cmds = [
-            "@SP",
-            "M=M-1",
-            "A=M",
-            "D=M",      # D = y
-            "@SP",
-            "M=M-1",
-            "A=M",
-            "D=D+M",    # D = y + x
-        ]
-        asm_cmds.extend(self.asm_push_d())
+    def _a_add(self):
+        asm_cmds = self._asm_pop_d()            # SP--;D=*M[SP]
+        asm_cmds.extend([
+            "@SP",                              # A=SP
+            "M=M-1",                            # SP--
+            "A=M",                              # A=M[SP]
+            "D=D+M",                            # D=D+*M[SP]
+        ])
+        asm_cmds.extend(self._asm_push_d())
         return asm_cmds
     
     # generates asm for the sub VM arithmetic command
-    def a_sub(self):
-        asm_cmds = [
-            "@SP",
-            "M=M-1",
-            "A=M",
-            "D=M",      # D = y
-            "D=-D",     # D = -y
-            "@SP",
-            "M=M-1",
-            "A=M",
-            "D=D+M"     # D = -y + x  (aka x + -y aka x - y)
-        ]
-        asm_cmds.extend(self.asm_push_d())
+    def _a_sub(self):
+        asm_cmds = self._asm_pop_d()            # SP--;D=*M[SP]
+        asm_cmds.extend([
+            "D=-D",                             # D=-D
+            "@SP",                              # A=SP
+            "M=M-1",                            # SP--
+            "A=M",                              # A=M[SP]
+            "D=D+M"                             # D=D+*M[SP]  (aka x + -y aka x - y)
+        ])
+        asm_cmds.extend(self._asm_push_d())
         return asm_cmds
     
-    def a_neg(self):
-        asm_cmds = [
-            "@SP",
-            "M=M-1",
-            "A=M",
-            "D=M",      # D = y
-            "D=-D",     # D = -y
-        ]
-        asm_cmds.extend(self.asm_push_d())
+    # generates asm for the neg VM arithmetic command
+    def _a_neg(self):
+        asm_cmds = self._asm_pop_d()            # SP--;D=*M[SP]
+        asm_cmds.extend([
+            "D=-D",                             # D=-D
+        ])
+        asm_cmds.extend(self._asm_push_d())
         return asm_cmds
     
+    # generates asm for the eq VM arithmetic command
     # if x - y == 0, they are equal.
-    def a_eq(self):
-        asm_cmds = []
-        asm_cmds.extend(self.a_sub())
-        asm_check_eq = [
+    def _a_eq(self):
+        asm_cmds = self._a_sub()
+        asm_cmds.extend([
             "@A_EQ_TRUE{}".format(self._a_eq_ctr),
             "D;JEQ",
-            "@SP",        # value is not == 0, store false (0)
-            "M=M-1",
-            "A=M",
-            "M=0",
-            "@SP",
-            "M=M+1",
+        ])
+        asm_cmds.extend(self._asm_push_false())
+        asm_cmds.extend([
             "@A_EQ_DONE{}".format(self._a_eq_ctr),
             "0;JMP",
             "(A_EQ_TRUE{})".format(self._a_eq_ctr),
-            "@SP",         # value is == 0, store true (-1)
-            "M=M-1",
-            "A=M",
-            "M=-1",
-            "@SP",
-            "M=M+1",
+        ])
+        asm_cmds.extend(self._asm_push_true())
+        asm_cmds.extend([
             "(A_EQ_DONE{})".format(self._a_eq_ctr),
-        ]
+        ])
         self._a_eq_ctr += 1
-        asm_cmds.extend(asm_check_eq)
         return asm_cmds
 
+    # generates asm for the gt VM arithmetic command
     # if x - y > 0, true.
-    def a_gt(self):
-        asm_cmds = []
-        asm_cmds.extend(self.a_sub())
-        asm_check_gt = [
+    def _a_gt(self):
+        asm_cmds = self._a_sub()
+        asm_cmds.extend([
             "@A_GT_TRUE{}".format(self._a_gt_ctr),
             "D;JGT",
-            "@SP",        # value is not == 0, store false (0)
-            "M=M-1",
-            "A=M",
-            "M=0",
-            "@SP",
-            "M=M+1",
+        ])
+        asm_cmds.extend(self._asm_push_false())
+        asm_cmds.extend([
             "@A_GT_DONE{}".format(self._a_gt_ctr),
             "0;JMP",
             "(A_GT_TRUE{})".format(self._a_gt_ctr),
-            "@SP",         # value is == 0, store true (-1)
-            "M=M-1",
-            "A=M",
-            "M=-1",
-            "@SP",
-            "M=M+1",
+        ])
+        asm_cmds.extend(self._asm_push_true())
+        asm_cmds.extend([
             "(A_GT_DONE{})".format(self._a_gt_ctr),
-        ]
+        ])
         self._a_gt_ctr += 1
-        asm_cmds.extend(asm_check_gt)
         return asm_cmds
 
+    # generates asm for the lt VM arithmetic command
     # if x - y < 0, true.
-    def a_lt(self):
-        asm_cmds = []
-        asm_cmds.extend(self.a_sub())
-        asm_check_lt = [
+    def _a_lt(self):
+        asm_cmds = self._a_sub()
+        asm_cmds.extend([
             "@A_LT_TRUE{}".format(self._a_lt_ctr),
             "D;JLT",
-            "@SP",        # value is not == 0, store false (0)
-            "M=M-1",
-            "A=M",
-            "M=0",
-            "@SP",
-            "M=M+1",
+        ])
+        asm_cmds.extend(self._asm_push_false())
+        asm_cmds.extend([
             "@A_LT_DONE{}".format(self._a_lt_ctr),
             "0;JMP",
             "(A_LT_TRUE{})".format(self._a_lt_ctr),
-            "@SP",         # value is == 0, store true (-1)
-            "M=M-1",
-            "A=M",
-            "M=-1",
-            "@SP",
-            "M=M+1",
+        ])
+        asm_cmds.extend(self._asm_push_true())
+        asm_cmds.extend([
             "(A_LT_DONE{})".format(self._a_lt_ctr),
-        ]
+        ])
         self._a_lt_ctr += 1
-        asm_cmds.extend(asm_check_lt)
         return asm_cmds
     
     # generates asm for the and VM arithmetic command
-    def a_and(self):
-        asm_cmds = [
-            "@SP",
-            "M=M-1",
-            "A=M",
-            "D=M",      # D = y
-            "@SP",
-            "M=M-1",
-            "A=M",
-            "D=D&M"     # D = x & y
-        ]
-        asm_cmds.extend(self.asm_push_d())
+    def _a_and(self):
+        asm_cmds = self._asm_pop_d()
+        asm_cmds.extend([
+            "@SP",                      # A=SP
+            "M=M-1",                    # SP--
+            "A=M",                      # A=*M[SP]
+            "D=D&M"                     # D=D&*M[SP]
+        ])
+        asm_cmds.extend(self._asm_push_d())
         return asm_cmds
 
     # generates asm for the or VM arithmetic command
-    def a_or(self):
-        asm_cmds = [
-            "@SP",
-            "M=M-1",
-            "A=M",
-            "D=M",      # D = y
-            "@SP",
-            "M=M-1",
-            "A=M",
-            "D=D|M"     # D = x & y
-        ]
-        asm_cmds.extend(self.asm_push_d())
+    def _a_or(self):
+        asm_cmds = self._asm_pop_d()
+        asm_cmds.extend([
+            "@SP",                      # A=SP
+            "M=M-1",                    # SP--
+            "A=M",                      # A=*M[SP]
+            "D=D|M"                     # D=D|*M[SP]
+        ])
+        asm_cmds.extend(self._asm_push_d())
         return asm_cmds
 
-    def a_not(self):
-        asm_cmds = [
-            "@SP",
-            "M=M-1",
-            "A=M",
-            "D=M",      # D = y
+    # generates asm for the not VM arithmetic command
+    def _a_not(self):
+        asm_cmds = self._asm_pop_d()
+        asm_cmds.extend([
             "D=!D",     # D = -y
-        ]
-        asm_cmds.extend(self.asm_push_d())
+        ])
+        asm_cmds.extend(self._asm_push_d())
         return asm_cmds
 
     # helper method to push a constant to the stack
-    def asm_push_constant(self, index):
+    def _asm_push_constant(self, index):
         asm_cmds = [
             "@{}".format(index),
             "D=A",
         ]
-        asm_cmds.extend(self.asm_push_d())
+        asm_cmds.extend(self._asm_push_d())
         return asm_cmds
     
     # helper method to push the value of the D register to the stack
-    def asm_push_d(self):
+    def _asm_push_d(self):
         asm_cmds = [
             "@SP",
             "A=M",
@@ -245,5 +208,38 @@ class CodeGenerator():
             "M=M+1",
         ]
         return asm_cmds
+    
+    # helper method to pop from the stack and store in the D register
+    def _asm_pop_d(self):
+        asm_cmds = [
+            "@SP",
+            "M=M-1",
+            "A=M",
+            "D=M",
+        ]
+        return asm_cmds
 
+    # helper method to push true (-1) onto the stack
+    def _asm_push_true(self):
+        asm_cmds = [
+            "@SP",
+            "M=M-1",
+            "A=M",
+            "M=-1",
+            "@SP",
+            "M=M+1",
+        ]
+        return asm_cmds
+
+    # helper method to push false (0) onto the stack
+    def _asm_push_false(self):
+        asm_cmds = [
+            "@SP",        
+            "M=M-1",
+            "A=M",
+            "M=0",
+            "@SP",
+            "M=M+1",
+        ]
+        return asm_cmds
 
