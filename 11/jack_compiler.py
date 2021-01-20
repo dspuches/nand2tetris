44a5cfb7c33e4bccb27e32f2aca6cb90b8cc5a3f
Dan Spuches <dspuches@gmail.com>
2021-01-19 21:34:46 -0500

    Changed jack_compiler to generate vm file instead of xml

--- 11/jack_compiler.py
+++ 11/jack_compiler.py
@@ -36,41 +36,25 @@ def main(input):
         exit(1)
 
     # loop through all .jack files
-    if args.tokenize:
-        for infile in infiles:
-            # only output tokenized xml output file
-            token_outfile_name = re.sub("\.jack$", "T.xml", infile)
-            token_outfile = os.path.join(outdir, token_outfile_name)
-            with open(token_outfile, "w") as out_f:
-                with open(infile) as in_f:
-                    t = JackTokenizer(in_f)
-                    out_f.write(t.token_xml_header())
-                    while (t.has_more_tokens()):
-                        t.advance()
-                        out_f.write(t.token_xml())
-                    out_f.write(t.token_xml_trailer())
-            
-    else:
-        # default is to generate output from the compilation engine
-        # each input file should have one and only one class
-        # so the only compilation engine function that should be called from
-        # here for each file is compileClass(). All other functions should be invoked recursively
-        # The output generated should be a single XML file
+    # generate vm output from the compilation engine
+    # each input file should have one and only one class
+    # so the only compilation engine function that should be called from
+    # here for each file is compileClass(). All other functions should be invoked recursively
+    # The output generated should be a single XML file
 
-        for infile in infiles:
-            outfile_name = re.sub("\.jack$", ".xml", infile)
-            outfile = os.path.join(outdir, outfile_name)
+    for infile in infiles:
+        outfile_name = re.sub("\.jack$", ".vm", infile)
+        outfile = os.path.join(outdir, outfile_name)
 
-            with open(outfile, "w") as out_f:
-                with open(infile) as in_f:
-                    try:
-                        engine = CompilationEngine(in_f, out_f)
-                    except SyntaxError as e:
-                        print("Syntax error at line {}. {}".format(e.line_num, e.message))
+        with open(outfile, "w") as out_f:
+            with open(infile) as in_f:
+                try:
+                    engine = CompilationEngine(in_f, out_f)
+                except SyntaxError as e:
+                    print("Syntax error at line {}. {}".format(e.line_num, e.message))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="input .jack file or directory containing .jack files")
-    parser.add_argument("--tokenize", help="generate tokenized output", action="store_true")
     args = parser.parse_args()
     main(args.input)
